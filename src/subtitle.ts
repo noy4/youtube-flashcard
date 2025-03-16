47612160cf33c63336a02d9187999e4e206fa7ff
noy4 <naoyan418@gmail.com>
2025-03-17 05:12:52 +0900

    sort

--- src/subtitle.ts
+++ src/subtitle.ts
@@ -3,10 +3,45 @@ import * as fs from 'node:fs'
 import path from 'node:path'
 import { parseSync } from 'subtitle'
 
-function parseSubs(content: string) {
-  return parseSync(content)
-    .filter(node => node.type === 'cue')
-    .map(v => v.data)
+/**
+ * Load subtitles from files or generate them using AI.
+ */
+export async function loadSubtitles(context: Context) {
+  const { options, paths } = context
+
+  const subs1Content = await loadSubtitle({
+    input: options.subs1,
+    output: paths.subs1,
+    // transcribe
+    generate: () => context.ai.transcribe({
+      audioPath: paths.video,
+      language: options.fromLang,
+    }),
+  })
+
+  const subs2Content = await loadSubtitle({
+    input: options.subs2,
+    output: paths.subs2,
+    // translate
+    generate: () => context.ai.translate({
+      content: subs1Content,
+      fromLang: options.fromLang,
+      toLang: options.toLang,
+    }),
+  })
+
+  const subs1 = parseSubs(subs1Content)
+  const subs2 = parseSubs(subs2Content)
+
+  const subtitles: Subtitle[] = []
+  for (const [index, sub] of subs1.entries()) {
+    subtitles.push({
+      ...sub,
+      translation: subs2[index].text,
+      audioPath: path.join(paths.segments, `segment_${index}.mp3`),
+    })
+  }
+  context.subtitles = subtitles
 }
 
 async function loadSubtitle(params: {
@@ -26,45 +61,8 @@ async function loadSubtitle(params: {
   }
 }
 
-// 文字起こしの読み込み
-export async function loadSubtitles(context: Context) {
-  const { options, paths } = context
-
-  const subs1Content = await loadSubtitle({
-    input: options.subs1,
-    output: paths.subs1,
-    generate: async () => {
-      const transcription = await context.ai.transcribe({
-        audioPath: paths.video,
-        language: options.fromLang,
-      })
-      return transcription
-    },
-  })
-
-  const subs2Content = await loadSubtitle({
-    input: options.subs2,
-    output: paths.subs2,
-    generate: async () => {
-      const translation = await context.ai.translate({
-        content: subs1Content,
-        fromLang: options.fromLang,
-        toLang: options.toLang,
-      })
-      return translation
-    },
-  })
-
-  const subs1 = parseSubs(subs1Content)
-  const subs2 = parseSubs(subs2Content)
-
-  const subtitles: Subtitle[] = []
-  for (const [index, sub] of subs1.entries()) {
-    subtitles.push({
-      ...sub,
-      translation: subs2[index].text,
-      audioPath: path.join(paths.segments, `segment_${index}.mp3`),
-    })
-  }
-  context.subtitles = subtitles
+function parseSubs(content: string) {
+  return parseSync(content)
+    .filter(node => node.type === 'cue')
+    .map(v => v.data)
 }

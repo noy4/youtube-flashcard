84e7ce122921a60f8dfab7009aac2e003a3cb54e
noy4 <naoyan418@gmail.com>
2025-04-01 15:39:38 +0900

    --add-to-anki false

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -19,7 +19,7 @@ const program = new Command()
   .option('--target-lang <code>', 'Target language code', 'en')
   .option('--native-lang <code>', 'Native language code', 'ja')
   // anki
-  .option('--add-to-anki', 'Add flashcards directly to Anki', true)
+  .option('--add-to-anki', 'Add flashcards directly to Anki')
   .option('--deck-name <name>', 'Anki deck name')
   .option('--model-name <name>', 'Anki model name', 'Basic')
   // api
